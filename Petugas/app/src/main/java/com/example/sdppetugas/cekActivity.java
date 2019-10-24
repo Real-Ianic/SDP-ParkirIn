8db4a311498d05c4011efd465c9b084154f323c2
dicky <dicky28aristo@gmail.com>
2019-10-24 21:04:14 +0700

    petugas belum selesai

--- Petugas/app/src/main/java/com/example/sdppetugas/cekActivity.java
+++ Petugas/app/src/main/java/com/example/sdppetugas/cekActivity.java
@@ -9,14 +9,41 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class cekActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+
+public class cekActivity extends AppCompatActivity {
+    ArrayList<String> arrOnGoing,arrPlat;
+    ArrayAdapter<String> platAdapter;
+    ListView lvPlat;
+    TextView tvPlat,tvNama;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cek);
+        lvPlat = findViewById(R.id.lvPlat);
+        tvNama = findViewById(R.id.tvNamaPlat);
+        tvPlat = findViewById(R.id.tvPlatNo);
+       if(getIntent()!=null){
+            arrOnGoing = getIntent().getStringArrayListExtra("ongoing");
+            arrPlat = getIntent().getStringArrayListExtra("platno");
+        }
+        platAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,arrPlat);
+        lvPlat.setAdapter(platAdapter);
+        lvPlat.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                tvNama.setText(arrOnGoing.get(i));
+                tvPlat.setText(arrPlat.get(i));
+            }
+        });
     }
     public void cekKendaraan(View v){
         Intent i = new Intent(this,statusKendaraan.class);
@@ -32,16 +59,13 @@ public class cekActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(item.getItemId()==R.id.optCek){
-            Toast.makeText(getApplicationContext(),"Ini halaman cek kendaraan",Toast.LENGTH_SHORT).show();
-        }
-        else if(item.getItemId()==R.id.optMasuk){
+        if(item.getItemId()==R.id.optOnGoing){
             Intent i = new Intent(this,MainActivity.class);
             finish();
             startActivity(i);
         }
-        else if(item.getItemId()==R.id.optStatus){
-            Intent i = new Intent(this,statusKendaraan.class);
+        else if(item.getItemId()==R.id.optLogout){
+            Intent i = new Intent(this,LoginActivity.class);
             finish();
             startActivity(i);
         }
