3c8dc234b9d7fa90eb0a2abc04eb23705d9bef81
fxaucky <auckyrh@gmail.com>
2019-11-15 06:54:31 +0700

    change admin

--- AdminFix/app/src/main/java/com/example/adminfix/MainActivity.java
+++ AdminFix/app/src/main/java/com/example/adminfix/MainActivity.java
@@ -8,18 +8,13 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button login;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        login=findViewById(R.id.btn_login);
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this,HomeActivity.class);
-                startActivity(i);
-            }
-        });
+
+
     }
 }
