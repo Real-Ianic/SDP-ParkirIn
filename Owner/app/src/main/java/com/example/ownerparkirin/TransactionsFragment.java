c239f2e0682306f6e56bb39fd3286c7087b38e64
Real-Ianic <adrian.ianic@gmail.com>
2019-12-13 01:23:03 +0700

    owner bisa load transaksi

--- Owner/app/src/main/java/com/example/ownerparkirin/TransactionsFragment.java
+++ Owner/app/src/main/java/com/example/ownerparkirin/TransactionsFragment.java
@@ -3,11 +3,20 @@ package com.example.ownerparkirin;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -15,6 +24,12 @@ import android.view.ViewGroup;
  */
 public class TransactionsFragment extends Fragment {
 
+    RecyclerView rvTrans;
+    TransaksiAdapter adapter;
+
+    MainActivity parentActivity;
+
+    ArrayList<classtransaksi> listTransaksi;
 
     public TransactionsFragment() {
         // Required empty public constructor
@@ -28,6 +43,19 @@ public class TransactionsFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_transactions, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
+        parentActivity = (MainActivity) getActivity();
 
+        rvTrans = view.findViewById(R.id.rvTrans);
+        adapter = new TransaksiAdapter(listTransaksi);
+        RecyclerView.LayoutManager lm = new LinearLayoutManager(getActivity());
+
+        rvTrans.setAdapter(adapter);
+        rvTrans.setLayoutManager(lm);
+        listTransaksi = (ArrayList<classtransaksi>)parentActivity.getTransactions();
+        adapter.notifyDataSetChanged();
+    }
 }
